866c1da7845ff861929cd62d5cec61670779b3f8
LaurenWorkosky <19lworkosky@student.dasd.org>
2018-07-24 14:44:47 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,5 +1,19 @@
 
 # Code your methods below
 def assume_stance
-  puts ""
+  puts "Plant legs far apart, bend knees slightly and keep posture loose"
 end
+
+def bust_a_move
+  puts "Be cool"
+  puts "Be snazzy"
+end
+
+def footwork
+  puts "Lift right foot"
+  puts "Return right foot to the ground"
+  puts "Finishing with a small skip-step backward"
+end
+
+def bob_the_reins
+  
